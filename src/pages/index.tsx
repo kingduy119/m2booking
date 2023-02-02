d261843ef8ee2a35339d9450b30bea08a72f6a14
= <=>
2023-02-02 15:04:56 +0700

    tailwindCSS configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,11 @@ import { todosActions } from "@/redux/slices/todos";
 import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
-  const dispatch = useAppDispatch();
-  const todos = useAppSelector((state) => state.todos);
-
-  if (todos.status === "idle") {
-    dispatch(todosActions.getEntities());
-  }
 
   return (
     <div>
       <h2>HOMEPAGE</h2>
-      <p>{JSON.stringify(todos.status)}</p>
-      <p>{JSON.stringify(todos.entities)}</p>
-      <p>{JSON.stringify(todos.error)}</p>
+      <div>Pro</div>
     </div>
   );
 };
